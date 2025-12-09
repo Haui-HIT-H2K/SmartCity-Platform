18f4737d82f5f22c44c9d86420ea5e8867f3009f
hoangnh4204 <hoangnh4204@gmail.com>
2025-12-09 21:35:11 +0700

    feat: Implement JWT-based authentication and authorization with Spring Security.

---
+++ backend/src/main/java/com/smartcity/dto/SignupRequest.java
@@ -0,0 +1,12 @@
+package com.smartcity.dto;
+
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class SignupRequest {
+    private String username;
+    private String password;
+    private Set<String> roles;
+}
